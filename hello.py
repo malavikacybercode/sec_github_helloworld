2ec6249d5eb6bd8d652c147369dc5be9c4419c5f
anandhu <anandhudj2231@gmail.com>
2023-01-09 14:10:20 +0530

    sum added

--- hello.py
+++ hello.py
@@ -4,3 +4,16 @@ print('added commit message!!!')
 
 print("my new edit")
 print("Learn New stuffs")
+
+print("latest update")
+
+def HiTeller():
+    return f" Malu is awesome"
+
+
+a = int(input("Enter number1 : -"))
+b = int(input("enter number2 : -"))
+
+print(f"Sum is {a+b}")
+
+# print(HiTeller())
